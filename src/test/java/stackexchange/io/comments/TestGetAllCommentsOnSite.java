f098c66b99b846a25ae9d320bed198336a7d2c5b
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2019-02-27 13:58:46 +0100

    Testing CI integration with Jenkins -4

--- src/test/java/stackexchange/io/comments/TestGetAllCommentsOnSite.java
+++ src/test/java/stackexchange/io/comments/TestGetAllCommentsOnSite.java
@@ -16,7 +16,7 @@ public class TestGetAllCommentsOnSite {
         assertResponse.canAssertResponseStatus(200,response.getStatusCode());
     }
     @Test
-    public void canGetAllCommentsOnSiteInAlphabeticalOrder4() {
+    public void canGetAllCommentsOnSiteInAlphabeticalOrder5() {
         Response response = commentsMethods.getReturnAllCommentsOnTheSite("comments","order=desc&sort=creation&filter=default&site=stackoverflow&run=true");
         assertResponse.canAssertResponseStatus(200,response.getStatusCode());
     }
