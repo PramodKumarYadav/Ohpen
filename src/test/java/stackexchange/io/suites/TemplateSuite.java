46f14f3defaee84d3c0bb9e723008ffd1d319584
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2019-02-28 12:07:44 +0100

    Added a suite class in tests. Added Profile in pom file and tested with command line OK.

---
+++ src/test/java/stackexchange/io/suites/TemplateSuite.java
@@ -0,0 +1,14 @@
+package stackexchange.io.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import stackexchange.io.comments.TestGetAllCommentsOnSite;
+import stackexchange.io.privileges.TestGetAllPrivileges;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestGetAllCommentsOnSite.class,
+        TestGetAllPrivileges.class
+})
+public class TemplateSuite {
+}
